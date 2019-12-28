61a9918f1602f5f919f7c767ac7a8ee297ba0239
Lovpreet Hansra <lhansra@ucsd.edu>
2019-12-28 13:34:35 -0800

    Initial Commit

---
+++ tipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalc
+//
+//  Created by Lovpreet Hansra on 12/28/19.
+//  Copyright © 2019 Lovpreet Hansra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
